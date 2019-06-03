fa0c32e7b92b606a4e9c65e9c223661b42826ef5
Justin Tew <jt@corporatetools.com>
2019-06-03 15:52:16 -0700

    updated setup

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+	singleQuote: true, //replace double quotes with singles, don't need doubles
+	semi: true, //use semicolons, keep it classic
+	useTabs: true, //use tabs, nobody got time for spaces
+	tabWidth: 4 //tabs width is 4 spaces, not 1, 2 or 3
+};
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # vscode-config
+
+## EXTENSIONS
+
+Apache Conf
+ESLint
+Material Theme
+Prettier
+Vetur
+
+## Setup
+
+1. Add extensions
+2. Add settings.json
+3. Add .prettierrc.js
+4. Go
